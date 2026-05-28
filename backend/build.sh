10a5dfbd130f51f64c4cce65f38f32ec9f83a4c4
Aditya Singh <aditya2112013@akgec.ac.in>
2026-05-29 03:37:32 +0530

    Auto-create admin user during build

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py seed_db
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin'); print('Admin user ready.')"
