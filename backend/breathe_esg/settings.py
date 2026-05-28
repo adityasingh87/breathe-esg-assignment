8c119e96b20fdc44dc4d2d76b568b9f2f62aa5e1
Aditya Singh <aditya2112013@akgec.ac.in>
2026-05-29 01:40:13 +0530

    Add production deployment config

--- backend/breathe_esg/settings.py
+++ backend/breathe_esg/settings.py
@@ -104,6 +104,7 @@ USE_TZ = True
 # Static files
 # ---------------------------------------------------------------------------
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # ---------------------------------------------------------------------------
 # Default primary key field type
